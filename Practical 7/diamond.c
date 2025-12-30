f6b657344c51c2cc6a418778669de377f5cc59f9
Abhijeet <harshanasane012@gmail.com>
2025-12-30 15:43:16 +0530

    7
    This program generates a diamond shape pattern based on the input number of rows.

---
+++ Practical 7/diamond.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+//Abhijeet //
+//RU 25 10040//
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        for(int s = 1; s <= n-i; s++)
+            printf(" ");
+        for(int j = 1; j <= 2*i-1; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    for(int i = n-1; i >= 1; i--) {
+        for(int s = 1; s <= n-i; s++)
+            printf(" ");
+        for(int j = 1; j <= 2*i-1; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
+//OUTPUT 
+
+// //Enter n: 5
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
+
+
